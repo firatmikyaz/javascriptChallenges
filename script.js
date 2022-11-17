09403c05ec540986db02f977e932277f951c67e4
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-11-17 23:28:58 +0300

    Coding Challenge #2 completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Coding Challenges #1
+/* // Coding Challenges #1
 console.log('----------  Coding Challenges #1');
 
 // BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
@@ -20,3 +20,38 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+// Coding Challenges #2
+// I copied my calculations from Challenge #1 for this challenge here.
+
+//Data 1
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+//Data 2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+//Control BMI value of Mark and John
+console.log(BMIMark, BMIJohn);
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI is higher than John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+}
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!"`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!"`);
+}
