02bfd70de9f6e77f4550a233d3bfd742c8afc519
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-11-18 22:54:08 +0300

    Coding Challenge #3 completed

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHigherBMI);
 */
 
-// Coding Challenges #2
+/* // Coding Challenges #2
 // I copied my calculations from Challenge #1 for this challenge here.
 
 //Data 1
@@ -54,4 +54,57 @@ if (markHigherBMI) {
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!"`);
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!"`);
+}
+*/
+
+// Coding Challenges #2
+
+// Data 1
+const avegareDolphins = (96 + 108 + 89) / 3;
+const averageKoalas = (88 + 91 + 110) / 3;
+
+// Bonus 1
+// const avegareDolphins = (97 + 112 + 101) / 3;
+// const averageKoalas = (109 + 95 + 123) / 3;
+
+// Bonus 2
+// const avegareDolphins = (97 + 112 + 101) / 3;
+// const averageKoalas = (109 + 95 + 106) / 3;
+
+console.log('---------- Task 1');
+console.log(
+    `Dolphins Avegare: ${avegareDolphins}, Koalas Average: ${averageKoalas}`,
+);
+
+console.log('---------- Task 2');
+if (avegareDolphins > averageKoalas) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (avegareDolphins < averageKoalas) {
+    console.log('Koalas win the trophy 🏆');
+} else {
+    console.log('Both win the thorpy 🏆');
+}
+
+console.log('---------- Bonus 1');
+if (avegareDolphins > averageKoalas && avegareDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (avegareDolphins < averageKoalas && averageKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else {
+    console.log('Both win the thorpy 🏆');
+}
+
+console.log('---------- Bonus 2');
+if (avegareDolphins > averageKoalas && avegareDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (avegareDolphins < averageKoalas && averageKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (
+    avegareDolphins === averageKoalas &&
+    avegareDolphins >= 100 &&
+    averageKoalas >= 100
+) {
+    console.log('Both win the thorpy 🏆');
+} else {
+    console.log('No one wins the thorpy 😭');
 }
