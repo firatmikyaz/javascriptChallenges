e30d1e7648781689107864d35c2430dd8f8aa99e
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-11-23 00:12:42 +0300

    Lecture-2 completed and lectura-1 updated

--- Js-Fundamentals-Part-1/content.md
+++ Js-Fundamentals-Part-1/content.md
@@ -7,3 +7,13 @@
 1. Declare variables called 'country', 'continent' and 'population' and
    assign their values according to your own country (population in millions)
 2. Log their values to the console
+
+## LECTURE: Data Types
+
+### Your tasks:
+
+1. Declare a variable called 'isIsland' and set its value according to your
+   country. The variable should hold a Boolean value. Also declare a variable
+   'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+   to the console
--- Js-Fundamentals-Part-1/script.js
+++ Js-Fundamentals-Part-1/script.js
@@ -1,8 +1,17 @@
 //// LECTURE: Values and Variables
 
-const country = 'moonlight';
-const continent = 'Europa';
-const population = 21;
+let country = 'moonlight';
+let continent = 'Europa';
+let population = 21;
 console.log(country); // moonlight
 console.log(continent); // Europa
 console.log(population); // 21
+
+//// LECTURE: Data Types
+
+let isIsland = true;
+let language;
+console.log(typeof country); // string
+console.log(typeof population); // number
+console.log(typeof isIsland); // boolean
+console.log(typeof language); // undefined
