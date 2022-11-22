fb11c0ca5d4306d4d0a14f5ff651a6b646526037
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-11-23 00:22:31 +0300

    Lecture-3 completed

--- Js-Fundamentals-Part-1/content.md
+++ Js-Fundamentals-Part-1/content.md
@@ -17,3 +17,13 @@
    'language', but don't assign it any value yet
 2. Log the types of 'isIsland', 'population', 'country' and 'language'
    to the console
+
+## LECTURE: let, const and var
+
+### Your tasks:
+
+1. Set the value of 'language' to the language spoken where you live (some
+   countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+   change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
--- Js-Fundamentals-Part-1/script.js
+++ Js-Fundamentals-Part-1/script.js
@@ -1,17 +1,25 @@
 //// LECTURE: Values and Variables
 
-let country = 'moonlight';
-let continent = 'Europa';
+// let country = 'moonlight';
+// let continent = 'Europa';
 let population = 21;
-console.log(country); // moonlight
-console.log(continent); // Europa
-console.log(population); // 21
+// console.log(country); // moonlight
+// console.log(continent); // Europa
+// console.log(population); // 21
 
 //// LECTURE: Data Types
 
-let isIsland = true;
+// const isIsland = true;
 let language;
-console.log(typeof country); // string
-console.log(typeof population); // number
-console.log(typeof isIsland); // boolean
-console.log(typeof language); // undefined
+// console.log(typeof country); // string
+// console.log(typeof population); // number
+// console.log(typeof isIsland); // boolean
+// console.log(typeof language); // undefined
+
+//// LECTURE: let, const and var
+
+language = 'moonlightse';
+const country = 'moonlight';
+const continent = 'Europa';
+const isIsland = false;
+// isIsland = true; // We get the error: 'Uncaught TypeError: Assignment to constant variable'
