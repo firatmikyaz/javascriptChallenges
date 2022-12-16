83a3cffcd18ef5f434d57b03f1f740ac8cc538d6
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-12-16 19:23:42 +0300

    Handling click events add project and manipulation element

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // for practices DOM manipulation, this write some code and we see the change elements text and value
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉CorrecT Number!';
@@ -7,3 +8,15 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    console.log(
+      (document.querySelector('.message').textContent = '⛔ No Number!')
+    );
+  }
+});
