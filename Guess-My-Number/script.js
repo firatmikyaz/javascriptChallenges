76c0d65b7a38f553d668935c466efd7566aa9e1d
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-12-16 20:51:53 +0300

    added code for 3 scenarios in the game

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -10,6 +10,11 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+// For testing
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -18,5 +23,25 @@ document.querySelector('.check').addEventListener('click', function () {
     console.log(
       (document.querySelector('.message').textContent = '⛔ No Number!')
     );
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉CorrecT Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too High...!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too Low...!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😔You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
