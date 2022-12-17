acc1ebb2ae6e4b393bf65a634818e66c0abcacca
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-12-17 22:46:46 +0300

    implementing highscore value

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -12,7 +12,7 @@ document.querySelector('.guess').value = 23;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// For testing
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,9 +28,15 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉CorrecT Number!';
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // highscore logic
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
