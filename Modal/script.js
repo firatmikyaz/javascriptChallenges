bdae77df548fffcc545ce61b55e784e57157fbaa
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-12-18 23:48:05 +0300

    We have added a function to activate the popup window when clicking the buttons and also to close it in 2 ways.

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  // Dom classList ile ekleme,çıkarma vb.işlem yaparız.
+  // Burada hidden'ı kaldır diyoruz ve pencere açılıyor.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
