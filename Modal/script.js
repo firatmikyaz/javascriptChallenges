5be8a8a65f842dfa9687d11fcfa5164144df1097
firatmikyaz <firat.mikyaz.tr@gmail.com>
2022-12-19 00:25:16 +0300

    We ensured that the popup window was closed when the Esc key was pressed.

--- Modal/script.js
+++ Modal/script.js
@@ -6,8 +6,8 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  // Dom classList ile ekleme,çıkarma vb.işlem yaparız.
-  // Burada hidden'ı kaldır diyoruz ve pencere açılıyor.
+  // Dom classList with add, remove, contains and other
+  // popup open when remove hidden class
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,3 +22,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // check control key
+  console.log(e.key);
+
+  // check control Esc buton and hidden class
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
